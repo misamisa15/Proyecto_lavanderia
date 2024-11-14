e25fb6101aba2dfedbb6871ade729561c44673eb
ari <aaspiazu8623@utm.edu.ec>
2024-11-13 23:55:24 -0500

    Pagina principal con cambios

--- app/routes.py
+++ app/routes.py
@@ -3,8 +3,17 @@ from flask import render_template, current_app as app
 @app.route('/')
 def index():
     buttons = [
-        {'icon': 'house', 'text': 'Inicio'},
-        {'icon': 'gear', 'text': 'Configuración'},
-        {'icon': 'person', 'text': 'Perfil'},
+        {'icon': 'person', 'text': 'Clientes'},
+        {'icon': 'database', 'text': 'Produc/Servicios'},
+        {'icon': 'file-earmark-check', 'text': 'Nueva Factura'},
+        {'icon': 'receipt', 'text': 'Comprobantes'},
+        {'icon': 'receipt-cutoff', 'text': 'Nueva Retención'},
+        {'icon': 'journal-text', 'text': 'Lista Retenciones'},
+        {'icon': 'truck', 'text': 'Guía de Remisión'},
+        {'icon': 'arrow-clockwise', 'text': 'Nota de Crédito'},
+        {'icon': 'file-earmark-minus', 'text': 'Nota de Débito'},
+        {'icon': 'journal-album', 'text': 'Liquidación de Compras'},
+        {'icon': 'file-earmark-text', 'text': 'Proformas'},
+        {'icon': 'cash-stack', 'text': 'Cuentas por Cobrar'}
     ]
     return render_template('pagina_principal.html', buttons=buttons)
