0be465db547ada9995cb3e04f58fd35aabc6edab
ari <aaspiazu8623@utm.edu.ec>
2024-11-14 14:41:29 -0500

    Nuevos cambios

--- app/routes.py
+++ app/routes.py
@@ -9,7 +9,6 @@ def index():
         {'icon': 'receipt', 'text': 'Comprobantes'},
         {'icon': 'receipt-cutoff', 'text': 'Nueva Retención'},
         {'icon': 'journal-text', 'text': 'Lista Retenciones'},
-        {'icon': 'truck', 'text': 'Guía de Remisión'},
         {'icon': 'arrow-clockwise', 'text': 'Nota de Crédito'},
         {'icon': 'file-earmark-minus', 'text': 'Nota de Débito'},
         {'icon': 'journal-album', 'text': 'Liquidación de Compras'},
@@ -17,3 +16,21 @@ def index():
         {'icon': 'cash-stack', 'text': 'Cuentas por Cobrar'}
     ]
     return render_template('pagina_principal.html', buttons=buttons)
+
+@app.route('/pagina_user.html')
+def paguser():
+    menu = [
+        {'text': 'Inicio'},
+        {'text': 'Turnos'},
+        {'text': 'Servicios'},
+        {'text': 'Compras'},
+        {'text': 'Facturas'}
+    ]
+    ima = [
+        
+        {'image': 'images/car3.jpg'},
+        {'image': 'images/car4.jpg'}
+    ]
+
+
+    return render_template('pagina_user.html',menu=menu, ima=ima)
